ab8f942ed27a2a6e2cc4246185cffabf472eab37
jaymin <jaymindesai766@gmail.com>
2017-07-05 01:53:17 +0530

    minor refactoring

--- src/main/java/com/services/application/EventController.java
+++ src/main/java/com/services/application/EventController.java
@@ -4,7 +4,6 @@ import com.services.domain.event.EventDto;
 import com.services.domain.event.EventService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
--- src/main/java/com/services/domain/event/converters/EventConverter.java
+++ src/main/java/com/services/domain/event/converters/EventConverter.java
@@ -2,6 +2,7 @@ package com.services.domain.event.converters;
 
 import com.services.domain.event.EventDto;
 import com.services.domain.event.Event;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,6 +10,7 @@ public class EventConverter {
 
     private final VenueConverter venueConverter;
 
+    @Autowired
     public EventConverter(VenueConverter venueConverter){
         this.venueConverter = venueConverter;
     }
